5a6aaa59185a2b22bbc42bcb3a1f5a368480cdaa
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-07-28 16:13:38 +0530

    working with core components and inline style

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello </Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text> Another piece of text!</Text>
+      </View>
+      <Text
+        style={{
+          margin: 16,
+          borderWidth: 2,
+          borderColor: "red",
+          borderRadius: 4,
+          padding: 4,
+        }}
+      >
+        Hello World!
+      </Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
