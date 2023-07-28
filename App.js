187edfc9abe2be774f607772b074fa9de74cf0a3
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-07-28 16:15:38 +0530

    uisng stylesheet object for making the style reusablity

--- App.js
+++ App.js
@@ -4,19 +4,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text> Another piece of text!</Text>
+        <Text style={styles.dummyText}> Another piece of text!</Text>
       </View>
-      <Text
-        style={{
-          margin: 16,
-          borderWidth: 2,
-          borderColor: "red",
-          borderRadius: 4,
-          padding: 4,
-        }}
-      >
-        Hello World!
-      </Text>
+      <Text style={styles.dummyText}>Hello World!</Text>
       <Button title="Tap me!" />
     </View>
   );
@@ -28,5 +18,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "red",
+    borderRadius: 4,
+    padding: 4,
   },
 });
